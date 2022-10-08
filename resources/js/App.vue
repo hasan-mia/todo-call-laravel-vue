b94ad3147d2eab66a14f274cc0dfa852bbc232d6
Md. Hasan Mia <hasanrafi69@gmail.com>
2022-10-09 01:56:04 +0600

    initial all setup with vue and tailwind

---
+++ resources/js/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1 class="text-center">{{ title }}</h1>
+</template>
+<script>
+export default {
+    setup: () => ({
+        title: 'How To Install Vue 3 in Laravel 9 From Scratch'
+    })
+}
+</script>
+
+<style>
+    
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
